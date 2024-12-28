87bf08973397f42ad708a0a8bbad72ed54047fef
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-12-28 12:59:47 +0500

    done

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Inter: ['"Inter", sans-serif'],
+    },
+  },
+};
